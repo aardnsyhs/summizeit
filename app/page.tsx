d751777f818ab591efb29a42f0a4715c8ffc99db
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-11 12:49:05 +0700

    feat: update Home page layout by adding background gradient and hero section components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import { Button } from "@/components/ui/button";
+import BgGradient from "@/components/common/BgGradient";
+import HeroSection from "@/components/home/HeroSection";
 
 export default function Home() {
   return (
-    <div className="">
-      <Button variant={"outline"} size={"lg"}>
-        Shadcn Button
-      </Button>
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
+      {/* Demo Section */}
+      {/* <HowItWorksSection /> */}
+
+      {/* Pricing Section */}
+      {/* <PricingSection /> */}
+
+      {/* CTA Section */}
+      {/* <CTASection /> */}
     </div>
   );
 }
