0e089015c018a32f891b7c277969f4313034621e
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-11 13:01:19 +0700

    feat: add DemoSection to Home page layout for enhanced user engagement

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 import BgGradient from "@/components/common/BgGradient";
+import DemoSection from "@/components/home/DemoSection";
 import HeroSection from "@/components/home/HeroSection";
-
 export default function Home() {
   return (
     <div className="relative w-full">
       <BgGradient />
       <div className="flex flex-col">
         <HeroSection />
+        <DemoSection />
       </div>
-      {/* Demo Section */}
-      {/* <HowItWorksSection /> */}
-
       {/* Pricing Section */}
       {/* <PricingSection /> */}
 
