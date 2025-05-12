7bdc0ac516562458c8b5e7f53d5af7b0ac9ded32
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-12 12:05:23 +0700

    feat: enhance UploadForm with validation and file input component

--- components/upload/UploadForm.tsx
+++ components/upload/UploadForm.tsx
@@ -1,3 +1,43 @@
+"use client";
+
+import { z } from "zod";
+import UploadFormInput from "./UploadFormInput";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid File" })
+    .refine(
+      (file) => file.size <= 20 * 1024 * 1024,
+      "File must be less than 20MB"
+    )
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File must be a PDF"
+    ),
+});
+
 export default function UploadForm() {
-  return <div>Upload Form</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+    const validatedFields = schema.safeParse({ file });
+
+    console.log(validatedFields);
+
+    if (!validatedFields.success) {
+      console.error(
+        validatedFields.error.flatten().fieldErrors.file?.[0] ?? "Invalid file"
+      );
+      return;
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
+      <UploadFormInput onSubmit={handleSubmit} />
+    </div>
+  );
 }
---
+++ components/upload/UploadFormInput.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+interface UploadFormInputProps {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export default function UploadFormInput({ onSubmit }: UploadFormInputProps) {
+  return (
+    <form action="" className="flex flex-col gap-6" onSubmit={onSubmit}>
+      <div className="flex justify-end items-center gap-1.5">
+        <Input
+          id="file"
+          name="file"
+          type="file"
+          accept="application/pdf"
+          className=""
+          required
+        />
+        <Button>Upload your PDF</Button>
+      </div>
+    </form>
+  );
+}
