9a387edadc2104348b56410bc5da15f99a14da15
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-16 16:03:49 +0700

    feat: integrate MotionDiv for enhanced animations in DashboardPage and Upload components

--- app/(logged-in)/upload/page.tsx
+++ app/(logged-in)/upload/page.tsx
@@ -1,7 +1,9 @@
 import BgGradient from "@/components/common/BgGradient";
+import { MotionDiv } from "@/components/common/MotionWrapper";
 import UploadForm from "@/components/upload/UploadForm";
 import UploadHeader from "@/components/upload/UploadHeader";
 import { hasReachedUploadLimit } from "@/lib/user";
+import { containerVariants } from "@/utils/constants";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -19,12 +21,17 @@ export default async function Page() {
   return (
     <section className="min-h-screen">
       <BgGradient />
-      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+      <MotionDiv
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8"
+      >
         <div className="flex flex-col items-center justify-center gap-6 text-center">
           <UploadHeader />
           <UploadForm />
         </div>
-      </div>
+      </MotionDiv>
     </section>
   );
 }
