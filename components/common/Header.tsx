fcf71ba5a01416590cd2f513805160550c4af9ed
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-16 19:54:29 +0700

    feat: enhance Header component with improved layout and user button positioning

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -5,7 +5,7 @@ import PlanBadge from "./PlanBadge";
 
 const Header = () => {
   return (
-    <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
+    <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto relative z-50">
       <div className="flex lg:flex-1">
         <NavLink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
           <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
@@ -22,12 +22,10 @@ const Header = () => {
       </div>
       <div className="flex lg:justify-end lg:flex-1">
         <SignedIn>
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-2 items-center relative z-50">
             <NavLink href="/upload">Upload a PDF</NavLink>
             <PlanBadge />
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+            <UserButton afterSwitchSessionUrl="/" />
           </div>
         </SignedIn>
         <SignedOut>
