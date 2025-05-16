e3214afa8582c6016e484df58a0ffc7b53669460
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-16 20:10:39 +0700

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import { ORIGIN_URL } from "@/utils/helpers";
 
 const fontSans = FontSans({
   variable: "--font-sans",
@@ -16,6 +17,17 @@ export const metadata: Metadata = {
   title: "SummizeIt - AI-Powered PDF Summarization",
   description:
     "SummizeIt helps you instantly extract key insights from any PDF document using advanced AI. Whether it's reports, research papers, or manuals—get concise summaries in seconds.",
+  openGraph: {
+    images: [
+      {
+        url: "/opengraph-image.png",
+      },
+    ],
+  },
+  metadataBase: new URL(ORIGIN_URL),
+  alternates: {
+    canonical: ORIGIN_URL,
+  },
 };
 
 export default function RootLayout({
